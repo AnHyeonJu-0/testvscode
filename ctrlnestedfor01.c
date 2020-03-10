a780060f6983017648d4bccb1356903fcda122f4
AnHyeonJu <61379962+AnHyeonJu-0@users.noreply.github.com>
2020-03-10 11:33:26 +0900

    Add files via upload

---
+++ ctrlnestedfor01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int i = 0, j =0;
+
+    for(i=0;i<5;++i)
+    {
+        for(j=0;j<5;++j)
+        {
+            printf("*\   ");
+        }
+        putchar("\n");
+    }
+    return 0;
+}
