b3ba122e30106c9a080908e9d21197504e7c0008
AnHyeonJu <61379962+AnHyeonJu-0@users.noreply.github.com>
2020-05-16 06:52:21 +0900

    Add files via upload

---
+++ errorsample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char szBuffer[12] = { "Helloworld" };
+	char* pszData = NULL;
+
+	pszData = (char*)malloc(sizeof(szBuffer) * 12);
+	memcpy(pszData, szBuffer, 12);
+	puts(pszData);
+	return 0;
+}
---
+++ ptrmemcpy01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//memcpy() 함수를 위한 헤더 포함
+#include <string.h>
+
+int main(void)
+{
+    char szBuffer[12] = { "HelloWorld"};
+    char szNewBuffer[12] = {0};
+
+    //원본에서 4바이트만 대상 메모리로 복사
+    memcpy(szNewBuffer, szBuffer,4);
+    puts(szNewBuffer);
+
+    //원본에서 6바이트만 대상 메모리로 복사
+    memcpy(szNewBuffer, szBuffer, 6);
+    puts(szNewBuffer);
+
+    //원본 메모리 전체를 대상 메모리로 복사
+    memcpy(szNewBuffer, szBuffer, sizeof(szBuffer));
+    puts(szNewBuffer);
+    return 0;
+}
